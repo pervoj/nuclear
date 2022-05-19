3996541d5eaa6f50c4a233a248e88e665ee8c488
Vojtěch Perník <develop@pervoj.cz>
2022-05-19 17:02:47 +0200

    Bigger update

--- src/js/navbar.js
+++ src/js/navbar.js
@@ -4,8 +4,10 @@ document.querySelectorAll('.navbar .toggler').forEach(el => {
 		menu.classList.toggle('open');
 		if (menu.classList.contains('open')) {
 			menu.style.maxHeight = menu.scrollHeight + 'px';
+			el.classList.add('open');
 		} else {
 			menu.style.maxHeight = null;
+			el.classList.remove('open');
 		}
 	});
 });
